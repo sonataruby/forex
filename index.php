585a2ea2e5153df426942665f10f744bec7836d7
Roller Davic <rollerdavic@gmail.com>
2019-11-06 13:37:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $login = $_GET["login"];
 $symbol = $_GET["symbol"];
 $date = $_GET["date"];
+$TimeServer = $_GET["TimeServer"];
 header('Content-Type: application/json');
 $data = [];
 
@@ -12,4 +13,7 @@ $data["29102019"]["XAUUSD"] = [
 ];
 $dataExport = [];
 $dataExport = $data[$date][$symbol];
+if($TimeServer){
+	file_put_contents(__DIR__."/date.txt", $TimeServer);
+}
 echo json_encode($dataExport);
