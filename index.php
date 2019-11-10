c67739d8ea6afa8b682ea608f41d0abc20a6b611
Roller Davic <rollerdavic@gmail.com>
2019-11-10 11:04:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,33 +11,71 @@
     <title>Magic AI Bot Render Serial</title>
   </head>
   <body>
-  	<div class="container">
-  		<?php if(isset($_POST["mt5id"]) && $_POST["mt5id"]){ ?>
-  			<div class="alert alert-success" role="alert">
-			  <b>Your Code : </b> <?php echo sha1($_POST["mt5id"]);?>
-			</div>
-  		<?php }else if(isset($_POST["mt5id"]) && $_POST["mt5id"] == ""){?>
-  			<div class="alert alert-danger" role="alert">
-			  <b>Error: </b> MT5 ID Empty
-			</div>
-  		<?php } ?>
-  		<h1>Enter MT5 ID</h1>
-  		<form method="post">
-  		  <div class="form-group">
-		    <label for="exampleInputMt51">MT5 ID</label>
-		    <input type="text" name="mt5id" class="form-control" id="exampleInputMt51" placeholder="MT5 ID">
+	<header>
+	  	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand" href="#">Navbar</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Link</a>
+		      </li>
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          Dropdown
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="#">Action</a>
+		          <a class="dropdown-item" href="#">Another action</a>
+		          <div class="dropdown-divider"></div>
+		          <a class="dropdown-item" href="#">Something else here</a>
+		        </div>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+		      </li>
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		    </form>
 		  </div>
-		  <div class="form-group">
-		    <label for="exampleInputEmail1">Email address</label>
-		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-		    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-		  </div>
-		  
-		  
-		  <button type="submit" class="btn btn-primary">Render Mac Serial</button>
-		</form>
-  	</div>
-    
+		</nav>
+	</header>
+	<div style="padding-top: 50px; padding-bottom: 50px; ">
+	  	<div class="container">
+	  		<?php if(isset($_POST["mt5id"]) && $_POST["mt5id"]){ ?>
+	  			<div class="alert alert-success" role="alert">
+				  <b>Your Code : </b> <?php echo sha1($_POST["mt5id"]);?>
+				</div>
+	  		<?php }else if(isset($_POST["mt5id"]) && $_POST["mt5id"] == ""){?>
+	  			<div class="alert alert-danger" role="alert">
+				  <b>Error: </b> MT5 ID Empty
+				</div>
+	  		<?php } ?>
+	  		<h1>Enter MT5 ID</h1>
+	  		<form method="post">
+	  		  <div class="form-group">
+			    <label for="exampleInputMt51">MT5 ID</label>
+			    <input type="text" name="mt5id" class="form-control" id="exampleInputMt51" placeholder="MT5 ID">
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Email address</label>
+			    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+			    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+			  </div>
+			  
+			  
+			  <button type="submit" class="btn btn-primary">Render Mac Serial</button>
+			</form>
+	  	</div>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
