138a1653a6de0555a2a72c18273c23632bf936b3
Roller Davic <rollerdavic@gmail.com>
2019-11-11 11:18:49 +0700

    Update buy.php

--- application/views/buy.php
+++ application/views/buy.php
@@ -1,4 +1,5 @@
 <div class="container">
 	<a href="#" class="btn btn-primary btn-lg">Đang bán với giá 2.800$ Full Version</a>
-	<?php include "./shadown.php";?>
+
 </div>
+<?php include __DIR__."/shadown.php";?>
