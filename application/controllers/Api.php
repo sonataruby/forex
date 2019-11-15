5c8efb4accacaa549c05cf9eebae7ef625ae8584
Roller Davic <rollerdavic@gmail.com>
2019-11-15 11:05:35 +0700

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 class Api extends MY_Controller {
 
 	public $channelId = '-1001179593062';
